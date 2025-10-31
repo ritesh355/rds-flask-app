b6f41632894280428d54f3bd8e56c69c832e7f1f
ritesh355 <riteshsingh86991@gmail.com>
2025-10-31 11:47:15 +0530

    RDS flask app

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+# ✅ Database connection details
+db = mysql.connector.connect(
+    host="database-1.ckf2cs480fnc.us-east-1.rds.amazonaws.com",  # Replace with your RDS endpoint
+    user="admin",
+    password="your_rds_password_here",
+    database="devopsdb"
+)
+
+@app.route('/')
+def home():
+    return "✅ Flask App Connected to AWS RDS!"
+
+@app.route('/users')
+def get_users():
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users;")
+    data = cursor.fetchall()
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
